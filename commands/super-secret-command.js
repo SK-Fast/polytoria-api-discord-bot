40d88359b9e88644cd2338d5ccb612ea3fa8ea34
SK-Fast <60780448+SK-Fast@users.noreply.github.com>
2021-11-06 17:49:28 +0700

    🤫

---
+++ commands/super-secret-command.js
@@ -0,0 +1,7 @@
+const {Client,MessageEmbed,Attachment} = require("discord.js")
+
+module.exports = function(message) {
+
+   message.author.send("SHHHHHH. This is very secret command!!! Don't tell anyone it exists!!!!!!!")
+
+}
