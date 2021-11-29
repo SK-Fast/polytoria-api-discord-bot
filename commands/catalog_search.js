723bcb7964fa31f735739a35ae0841c34b7c213e
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2021-11-29 08:12:46 +0700

    fixe time stampa thing

--- commands/catalog_search.js
+++ commands/catalog_search.js
@@ -1,13 +1,14 @@
 const {Client,MessageEmbed,Attachment} = require("discord.js")
+const Converter = require('timestamp-conv');
 
-module.exports = function(message,args) {
+module.exports = function(message,args,botinfo) {
 
   const {RequestAPIJSON,RequestAPINormal,AxiosRequestAPIJSON} = require("../bot_modules/FetchService.js")
   const StartType = require("../bot_modules/TypingService.js")
 
     if (!args[1]) { message.channel.send("Missing args 1 | Please type what do you want to search!"); return }
     StartType(message)
-    let thing2search = message.content.replace('p!catalog-search ', '').replace(/ /g, '%20') // Cut off command from thing that user want to search
+    let thing2search = message.content.replace(botinfo["Prefix"] + 'catalog-search ', '').replace(/ /g, '%20') // Cut off command from thing that user want to search
 
     RequestAPIJSON('https://api.polytoria.com/v1/asset/catalog?page=0&q=' + thing2search,function(data,statuscode) {
       if (typeof(data) == "undefined") { 
@@ -53,8 +54,8 @@ module.exports = function(message,args) {
         .setURL("https://polytoria.com/shop/" + locatedthing["id"])
         .setDescription(proceesscreator + "\n" + processmessage + "\n" + processemoji + " " + locatedthing["price"] + "\n\n" + buytext)
         .addFields(
-          { name: 'Created At', value: `<t:${data2["CreatedAt"]}>`,inline: true },
-          { name: 'Updated At', value: `<t:${data2["UpdatedAt"]}>`,inline: true },
+          { name: 'Created At', value: `${new Converter.timestamp(data2["CreatedAt"]).formatDay.replace(".", "/").replace(".", "/")}`,inline: true },
+          { name: 'Updated At', value: `${new Converter.timestamp(data2["UpdatedAt"]).formatDay.replace(".", "/").replace(".", "/")}`,inline: true },
           { name: 'Sales', value: data2["Sales"],inline: false },
           { name: 'Favourites', value: data2["Favorites"],inline: true },
           )
