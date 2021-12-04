420a60498c2f0b7d5d5acefc7f014976680f9d6b
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2021-12-04 13:54:57 +0700

    Added IsNumber Detection

--- commands/brick2stud.js
+++ commands/brick2stud.js
@@ -1,8 +1,14 @@
 const {Client,MessageEmbed,Attachment} = require("discord.js")
+const isNaN = require('is-nan');
 
 module.exports = function(message,args) {
 
     if (!args[1]) {return message.channel.send("Missing args 1 | Please select amount!")}
+
+    if (isNaN(parseInt(args[1]))) {
+        message.channel.send("`Failure` The provided amount is not number.")
+        return
+    }
 
     let Calculated = Math.round(Number(args[1]) * 15)
 
