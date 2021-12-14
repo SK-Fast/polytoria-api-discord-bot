97aa3af7aac09662b829e045305d95fd9a403804
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2021-12-14 09:07:50 +0700

    tEH epIC statUs!!!

---
+++ commands/status.js
@@ -0,0 +1,44 @@
+const {Client,MessageEmbed,Attachment} = require("discord.js")
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = function(message,args) {
+
+  const StartType = require("../bot_modules/TypingService.js")
+  const getRandomInt = require("../bot_modules/RandomService.js")
+
+  let StatusData = {
+    Site: 0,
+    Outernal: 0,
+    Internal: 0,
+  }
+
+  function ProceedData() {
+    let embed = new MessageEmbed()
+.setColor('#fe5953')
+  .setTitle('Polytoria Server Status')
+ .setDescription(`
+ ${StatusData["Site"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Site"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Site Status(${StatusData["Site"]})
+ ${StatusData["Outernal"].toString().startsWith("2")  ? "<:check:920132963608457266>" : StatusData["Outernal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Outernal API Status(${StatusData["Outernal"]})
+ ${StatusData["Internal"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Internal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Internal API Status(${StatusData["Internal"]})
+ `)
+
+ .setFooter("Something went wrong? Please report it here: https://github.com/SK-Fast/polytoria-api-discord-bot/issues/new")
+
+    message.channel.send("",embed)
+  }
+
+  fetch("https://polytoria.com/").then(sitedata => {
+    StatusData["Site"] = sitedata.status
+   
+    fetch("https://api.polytoria.com/v1/asset/info?id=50").then(assetdata => {
+      StatusData["Outernal"] = assetdata.status
+
+      fetch("https://polytoria.com/api/fetch/user/clientinfo?username=DevPixels").then(interdata => {
+        StatusData["Internal"] = interdata.status
+
+        ProceedData()
+      })
+    })
+
+ })
+}
