b0603753cada986ebc40388097c1dfb9a94840c6
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2021-12-17 22:43:09 -0600

    Bumped bots version - 1.10 > 1.20

--- commands/version.js
+++ commands/version.js
@@ -8,7 +8,7 @@ module.exports = function(message) {
      .setColor('#fe5953')
        .setTitle('Information regarding the Bot')
        .addField("We are redoing the bot!", "The Polytoria Staff Team + DevPixels will be redoing every command to be even better than before. As a result for this, we will no longer be providing major updates to the bot until we are done with our reconstruction. (We will update the bot if there is an API change, don't worry!) Thank you for understanding.")
-       .addField("Who does this apply?", "This applies for everyone running our bot using version 1.10 and below. The big update will be 2.0.0, with all kinds of new things and big changes.")
+       .addField("Who does this apply?", "This applies for everyone running our bot using version 1.20 and below. The big update will be 2.0.0, with all kinds of new things and big changes.")
         .setFooter("Made by DevPixels and bags. Contact us at DevPixels#5746, StarManTheGamer#0001")
 
 
