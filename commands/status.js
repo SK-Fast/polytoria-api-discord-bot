36d46415a57fcc3e681448e28f0d6cd082d779b5
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2022-01-16 15:46:14 -0600

    Added Docs + Corp to status

--- commands/status.js
+++ commands/status.js
@@ -10,6 +10,8 @@ module.exports = function(message,args) {
     Site: 0,
     Outernal: 0,
     Internal: 0,
+    Corp: 0,
+    Docs: 0,
   }
 
   function ProceedData() {
@@ -17,9 +19,11 @@ module.exports = function(message,args) {
 .setColor('#fe5953')
   .setTitle('Polytoria Server Status')
  .setDescription(`
- ${StatusData["Site"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Site"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Site Status(${StatusData["Site"]})
- ${StatusData["Outernal"].toString().startsWith("2")  ? "<:check:920132963608457266>" : StatusData["Outernal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Outernal API Status(${StatusData["Outernal"]})
- ${StatusData["Internal"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Internal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Internal API Status(${StatusData["Internal"]})
+ ${StatusData["Site"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Site"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Site Status (${StatusData["Site"]})
+ ${StatusData["Outernal"].toString().startsWith("2")  ? "<:check:920132963608457266>" : StatusData["Outernal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Outernal API Status (${StatusData["Outernal"]})
+ ${StatusData["Internal"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Internal"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Internal API Status (${StatusData["Internal"]})
+ ${StatusData["Corp"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Corp"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Corporate Website (${StatusData["Corp"]})
+ ${StatusData["Docs"].toString().startsWith("2") ? "<:check:920132963608457266>" : StatusData["Docs"] == 503 ? "<:warning:920132963461627964>" : "<:error:920132962887028788>"}  Documents Page (${StatusData["Docs"]})
  `)
 
  .setFooter("Something went wrong? Please report it here: https://github.com/SK-Fast/polytoria-api-discord-bot/issues/new")
@@ -27,7 +31,7 @@ module.exports = function(message,args) {
     message.channel.send("",embed)
   }
 
-  fetch("https://polytoria.com/").then(sitedata => {
+  fetch("https://polytoria.com").then(sitedata => {
     StatusData["Site"] = sitedata.status
    
     fetch("https://api.polytoria.com/v1/asset/info?id=50").then(assetdata => {
@@ -36,9 +40,17 @@ module.exports = function(message,args) {
       fetch("https://polytoria.com/api/fetch/user/clientinfo?username=DevPixels").then(interdata => {
         StatusData["Internal"] = interdata.status
 
+        fetch("https://corp.polytoria.com").then(interdata => {
+        StatusData["Corp"] = interdata.status
+
+        fetch("https://docs.polytoria.com").then(interdata => {
+          StatusData["Docs"] = interdata.status
+
         ProceedData()
       })
     })
 
  })
+})
+})
 }
